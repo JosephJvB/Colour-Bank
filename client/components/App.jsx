4cd32733ef967b62c3955f4c2fb7ff9008e306e1
JosephJvB <jo-van-bo@hotmail.com>
2018-01-21 15:00:32 +1300

    figured out hyperscript routing :smile:

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,6 @@
 import { Fragment } from 'react'
 import h from 'react-hyperscript'
+import { Route } from 'react-router-dom'
 
 import ColourBox from './ColourBox'
 import Informant from './Informant'
@@ -9,11 +10,17 @@ const App = () => {
   const cols = ['red', 'blue', 'yellow', 'green']
   return (
     h(Fragment, [
-      h('div', { 'className': 'columns has-text-centered' }, [
-        rgbs.map((c, i) => h(ColourBox, { 'key': i, 'rgb': c, 'col': cols[i] }))
-      ]),
-      cols.length > 100 && h(Fragment, 'this is how u conditional???'),
-      h(Informant)
+      h(Route, {'path': '/',
+        'exact': true,
+        'component': () => h('div', { 'className': 'columns has-text-centered' },
+          rgbs.map((c, i) => h(ColourBox, { 'key': i, 'rgb': c, 'col': cols[i] }))
+        )
+      }
+      ),
+      h(Route, {'path': '/wizard1',
+        'component': Informant
+      }
+      )
     ])
   )
 }
