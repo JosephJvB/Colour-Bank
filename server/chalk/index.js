d5e2d4187a7f5681194287987091a9a37308c8c7
JosephJvB <jo-van-bo@hotmail.com>
2018-01-23 11:48:54 +1300

    3/10 rainbow parrot

--- server/chalk/index.js
+++ server/chalk/index.js
@@ -1,37 +1,17 @@
-const chalk = require('chalk')
+const fs = require('mz/fs')
 const { rainbow } = require('chalk-animation')
+// setInterval( x, 70)
 
-const { log } = global.console
+birdLoop()
 
-// tetrachromacy()
-
-function tetrachromacy () {
-  log(chalk.red.bgWhite('i believe'))
-  log(chalk.blue.bgYellow('i see'))
-  log(chalk.yellow.bgBlue('the four'))
-  log(chalk.white.bgRed('colours'))
-}
-// const col = ['red', 'blue', 'yellow']
-// col.forEach(c => log(chalk[c]))
-
-// cookiecutter()
-
-function cookiecutter () {
-  log(`
-  CPU: ${chalk.red('90%')}
-  RAM: ${chalk.green('40%')}
-  DISK: ${chalk.yellow('70%')}
-  `)
+function birdLoop () {
+  let idx = 0
+  setInterval(() => {
+    if (idx === 10) idx = 0
+    fs.readFile(`${__dirname}/birds/${idx}.txt`, 'utf8')
+      .then(bird => rainbow(bird))
+    idx++
+  }, 70)
 }
 
-// keyss()
-
-function keyss () {
-  log(chalk.keyword('orange').underline('this text is orange'))
-}
-
-darkside()
-
-function darkside () {
-  rainbow('joseph and his amazing technicolour dreamcoat')
-}
+// not great but there's something there lol
