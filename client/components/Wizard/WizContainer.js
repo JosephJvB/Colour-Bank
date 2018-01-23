270778d1ac5659eaa1405f4567cc145d7e65d658
JosephJvB <jo-van-bo@hotmail.com>
2018-01-23 16:37:42 +1300

    basics to navigate form pages

--- client/components/Wizard/WizContainer.js
+++ client/components/Wizard/WizContainer.js
@@ -1,16 +1,39 @@
-import { Fragment } from 'react'
+import { Fragment, Component } from 'react'
 import h from 'react-hyperscript'
 
 import WizOne from './WizOne'
 
-const WizContainer = props => {
-  return (
-    h(Fragment, [
-      'hi im wizcon',
-      h('br'),
-      h(WizOne)
-    ])
-  )
+class WizContainer extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { formNo: 1 }
+    // binds here
+  }
+  // functions here
+
+  nextPage () {
+    const { formNo } = this.state
+    this.setState({ formNo: formNo + 1 })
+  }
+
+  prevPage () {
+    const { formNo } = this.state
+    this.setState({ formNo: formNo - 1 })
+  }
+
+  render () {
+    const { formNo } = this.state
+    const { nextPage, prevPage } = this
+    return (
+      h(Fragment, [
+        'hi im wizcon',
+        h('br'),
+        formNo === 1 && h(WizOne, { next: nextPage })
+        // formNo === 2 && h(WizTwo)
+        // formNo === 3 && h(WizThree)
+      ])
+    )
+  }
 }
 
 // Wizard = connect(state => {
