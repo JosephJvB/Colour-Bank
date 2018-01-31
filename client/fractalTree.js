3e7d98b49047d9a8045d310a6ecfb9c26104f2bc
JosephJvB <jo-van-bo@hotmail.com>
2018-01-31 19:05:25 +1300

    back on the fractal track, who's WEBGL is this :joy:

--- client/fractalTree.js
+++ client/fractalTree.js
@@ -1,26 +1,17 @@
+const { log } = global.console
+
 const w = window.screen.availWidth / 2
 const h = window.screen.availHeight / 2
-const b = w / 5
-
-const { log } = global.console
+const b = w / 2
 
 export default function fractalTree (p) {
   p.setup = () => {
-    // log()
-    p.createCanvas(w, h, p.WEBGL)
+    // log(p)
+    p.createCanvas(w, h)
   }
   p.draw = () => {
     p.background(51)
     p.stroke(255)
-    p.line(b, h, b, h - 200)
-    p.translate(200, h)
-    // branch(100, p)
+    p.line(b, h, b, h - 100)
   }
-}
-
-function branch (len, p) {
-  p.line(0, 0, 0, -len)
-  p.translate(0, -len)
-  p.rotate(p.PI / 4)
-  // p.line(0, 0, 0, -len)
 }
