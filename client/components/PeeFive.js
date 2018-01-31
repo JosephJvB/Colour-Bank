25a313d27a79e70409b725eb7058ecaff91b95e4
JosephJvB <jo-van-bo@hotmail.com>
2018-01-31 19:50:28 +1300

    starting to look like a sketchbook nice

--- client/components/PeeFive.js
+++ client/components/PeeFive.js
@@ -1,6 +1,7 @@
 import { Fragment as F } from 'react'
 import h from 'react-hyperscript'
 import P5Wrapper from 'react-p5-wrapper'
+import { Link } from 'react-router-dom'
 
 import sketch from '../sketch'
 import fractalTree from '../fractalTree'
@@ -8,9 +9,14 @@ import fractalTree from '../fractalTree'
 const PeeFive = () => {
   return (
     h(F, [
-      'im p5',
-      // h(P5Wrapper, { sketch })
-      h(P5Wrapper, { sketch: fractalTree })
+      h(Link, { to: '/' }, [
+        h('button', { className: 'button is-success' }, 'home :)')
+      ]),
+      h(F, 'enjoy da P5 m\'dude'),
+      h('div', { className: 'columns', style: { marginTop: 20 } }, [
+        h(P5Wrapper, { sketch: fractalTree }),
+        h(P5Wrapper, { sketch })
+      ])
       // so basically the P5Wrapper takes a vanilla js file and is able to render it inside react framework. pretty neat
     ])
   )
--- client/fractalTree.js
+++ client/fractalTree.js
@@ -5,34 +5,40 @@ const h = window.screen.availHeight / 2
 const b = w / 2
 
 export default function fractalTree (p) {
-  const angle = p.PI / 4
+  const angle = p.PI / 6
 
+  // P5 setup function
   p.setup = () => {
-    log(p)
+    // log(p)
     p.createCanvas(w, h)
   }
+
+  // P5 draw function
   p.draw = () => {
     p.background(51)
     p.stroke(255)
     p.translate(b, h)
-    branch(150)
+    branch(120)
   }
 
+  // custom recursive branch function
   const branch = (len) => {
     // first branch
     p.line(0, 0, 0, -len)
     // dont make teeny branches
-    if (len < 5) return
+    if (len < 10) return
     // right branch child
     p.translate(0, -len)
     p.push()
     p.rotate(angle)
-    branch(len * 0.6)
+    branch(len * 0.7)
     p.pop()
     // left branch child
     p.push()
     p.rotate(-angle)
-    branch(len * 0.6)
+    branch(len * 0.7)
     p.pop()
   }
 }
+
+// want to make a slider like old mate, do I have access to the p5 dom library with my wrapper?
