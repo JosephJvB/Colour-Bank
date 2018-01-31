9b9f57484f90ea58ed946259bc652dfeaef0e9a3
JosephJvB <jo-van-bo@hotmail.com>
2018-01-31 19:32:28 +1300

    ze most basic fractal tree

--- client/fractalTree.js
+++ client/fractalTree.js
@@ -5,13 +5,34 @@ const h = window.screen.availHeight / 2
 const b = w / 2
 
 export default function fractalTree (p) {
+  const angle = p.PI / 4
+
   p.setup = () => {
-    // log(p)
+    log(p)
     p.createCanvas(w, h)
   }
   p.draw = () => {
     p.background(51)
     p.stroke(255)
-    p.line(b, h, b, h - 100)
+    p.translate(b, h)
+    branch(150)
+  }
+
+  const branch = (len) => {
+    // first branch
+    p.line(0, 0, 0, -len)
+    // dont make teeny branches
+    if (len < 5) return
+    // right branch child
+    p.translate(0, -len)
+    p.push()
+    p.rotate(angle)
+    branch(len * 0.6)
+    p.pop()
+    // left branch child
+    p.push()
+    p.rotate(-angle)
+    branch(len * 0.6)
+    p.pop()
   }
 }
